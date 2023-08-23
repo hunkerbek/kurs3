fe3a105e1c16b9be0cee9ab2f05e88bf6873030d
hunke <hunker.90@mail.ru>
2023-08-23 19:33:24 +0300

    add get_executed_operations

--- utils.py
+++ utils.py
@@ -4,3 +4,12 @@ def load_file(filename):
     with open(filename, encoding="utf-8") as file:
         return json.load(file)
 
+
+def get_executed_operations(list_operations):
+    result_list = []
+    for x in list_operations:
+        if x and x["state"] == "EXECUTED":
+            result_list.append(x)
+    return result_list
+
+
